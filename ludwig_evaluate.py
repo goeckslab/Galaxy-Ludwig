336365692d0cee065041079586dafd9096ce68a3
qiagu <guqiang01@gmail.com>
2022-01-21 18:39:19 -0800

    Add ludwig_evaluate tool

---
+++ ludwig_evaluate.py
@@ -0,0 +1,13 @@
+import logging
+import pickle
+import sys
+
+from model_unpickler import SafeUnpickler
+from ludwig.evaluate import cli
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+setattr(pickle, 'Unpickler', SafeUnpickler)
+
+cli(sys.argv[1:])
