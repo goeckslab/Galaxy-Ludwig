e151c2cedab17f5f84cdfbce29b0f6551a181dd4
qiagu <guqiang01@gmail.com>
2022-11-03 22:48:19 -0700

    Use ludwig contants

--- tools/ludwig_render_config.py
+++ tools/ludwig_render_config.py
@@ -1,6 +1,15 @@
 import json
 import sys
 
+from ludwig.constants import (
+    COMBINER,
+    HYPEROPT,
+    INPUT_FEATURES,
+    MODEL_TYPE,
+    OUTPUT_FEATURES,
+    PROC_COLUMN,
+    TRAINER,
+)
 from ludwig.utils.defaults import merge_with_defaults
 
 import yaml
@@ -12,25 +21,25 @@ with open(inputs, 'r') as handler:
 
 config = {}
 # input features
-config['input_features'] = []
-for ftr in params['input_features']['input_feature']:
-    config['input_features'].append(ftr['input_feature_selector'])
+config[INPUT_FEATURES] = []
+for ftr in params[INPUT_FEATURES]['input_feature']:
+    config[INPUT_FEATURES].append(ftr['input_feature_selector'])
 
 # output features
-config['output_features'] = []
-for ftr in params['output_features']['output_feature']:
-    config['output_features'].append(ftr['output_feature_selector'])
+config[OUTPUT_FEATURES] = []
+for ftr in params[OUTPUT_FEATURES]['output_feature']:
+    config[OUTPUT_FEATURES].append(ftr['output_feature_selector'])
 
 # combiner
-config['combiner'] = params['combiner']
+config[COMBINER] = params[COMBINER]
 
 # training
-config["trainer"] = params["trainer"]["trainer"]
-config['model_type'] = config['trainer'].pop("model_type")
+config[TRAINER] = params[TRAINER][TRAINER]
+config[MODEL_TYPE] = config[TRAINER].pop(MODEL_TYPE)
 
 # hyperopt
-if params['hyperopt']['do_hyperopt']:
-    config['hyperopt'] = params['hyperopt']['hyperopt']
+if params[HYPEROPT]['do_hyperopt']:
+    config[HYPEROPT] = params[HYPEROPT][HYPEROPT]
 
 with open('./pre_config.yml', 'w') as f:
     yaml.dump(config, f, allow_unicode=True, default_flow_style=False)
@@ -40,10 +49,10 @@ output_config = merge_with_defaults(config)
 
 
 def clean_proc_column(config: dict) -> None:
-    for ftr in config["input_features"]:
-        ftr.pop("proc_column", None)
-    for ftr in config["output_features"]:
-        ftr.pop("proc_column", None)
+    for ftr in config[INPUT_FEATURES]:
+        ftr.pop(PROC_COLUMN, None)
+    for ftr in config[OUTPUT_FEATURES]:
+        ftr.pop(PROC_COLUMN, None)
 
 
 clean_proc_column(output_config)
