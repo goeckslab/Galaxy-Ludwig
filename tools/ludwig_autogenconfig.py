66a0e9b55441279509a2ca349598a72737028fb8
JunhaoQiu <56094690+qchiujunhao@users.noreply.github.com>
2024-04-04 21:08:01 -0400

    added a checked box for rendered config

--- tools/ludwig_autogenconfig.py
+++ tools/ludwig_autogenconfig.py
@@ -12,11 +12,15 @@ def main():
     parser.add_argument('--dataset', type=str, help='Path to the dataset file', required=True)
     parser.add_argument('--output_feature', type=str, help='Name for the output feature', required=True)
     parser.add_argument('--output', type=str, help='Path for the output file', required=True)
+    parser.add_argument('--renderconfig', action='store_true', help='Render the config', required=False, default=False)
     args = parser.parse_args()
 
-    
     args_init = ["--dataset", args.dataset, "--target", args.output_feature, "--output", args.output]
     automl.cli_init_config(args_init)
+
+    if args.renderconfig:
+        args_render = ["--config", args.output, "--output", args.output]
+        defaults.cli_render_config(args_render)
 
 if __name__ == "__main__":
     main()
