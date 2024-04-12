7d91e58ed9bb2e12c57dc3c54397cff144361011
JunhaoQiu <56094690+qchiujunhao@users.noreply.github.com>
2024-04-12 13:05:44 -0400

    added a dropdown for selecting
    the output feature

--- tools/ludwig_autogenconfig.py
+++ tools/ludwig_autogenconfig.py
@@ -4,18 +4,25 @@ import logging
 from ludwig import automl
 from ludwig.utils import defaults
 
+from pandas import read_csv
+
 logging.basicConfig(level=logging.DEBUG)
 LOG = logging.getLogger(__name__)
 
 def main():
     parser = argparse.ArgumentParser(description='Render a Ludwig config')
     parser.add_argument('--dataset', type=str, help='Path to the dataset file', required=True)
-    parser.add_argument('--output_feature', type=str, help='Name for the output feature', required=True)
+    parser.add_argument('--output_feature', type=int, help='Name for the output feature', required=True)
     parser.add_argument('--output', type=str, help='Path for the output file', required=True)
     parser.add_argument('--renderconfig', action='store_true', help='Render the config', required=False, default=False)
     args = parser.parse_args()
 
-    args_init = ["--dataset", args.dataset, "--target", args.output_feature, "--output", args.output]
+    # get the output feature name
+    df = read_csv(args.dataset, nrows=2, sep=None, engine='python') 
+    names = df.columns.tolist()
+    target = names[args.output_feature-1]
+    
+    args_init = ["--dataset", args.dataset, "--target", target, "--output", args.output]
     automl.cli_init_config(args_init)
 
     if args.renderconfig:
