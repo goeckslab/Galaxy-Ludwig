042a2a679ee062fece69bcd8c3c53c22e3587005
JunhaoQiu <56094690+qchiujunhao@users.noreply.github.com>
2025-01-07 10:13:36 -0500

    fixed issues after testing on the cancer server  (#20)
    * fixed issues after testing on the cancer server
    
    1. The Ray server requires `TMP` environment variables to start.
    
    2. Sometimes datasets have spaces at the end of their filenames. During sanitization, these spaces cause the name to become `**.csv_`, which raises an issue. To fix this, spaces in the filename are replaced with nothing before sanitization.
    
    3.Removed the table in the hyperopt HTML report since it was too visually unappealing. A JSON file output is provided instead.
    
    * test data updated
    
    * python lint
    
    * replace -> strip

--- tools/ludwig_hyperopt.py
+++ tools/ludwig_hyperopt.py
@@ -1,4 +1,3 @@
-import json
 import logging
 import os
 import pickle
@@ -15,8 +14,7 @@ from model_unpickler import SafeUnpickler
 from utils import (
     encode_image_to_base64,
     get_html_closing,
-    get_html_template,
-    json_to_html_table
+    get_html_template
 )
 
 logging.basicConfig(level=logging.DEBUG)
@@ -31,17 +29,25 @@ cli(sys.argv[1:])
 def generate_html_report(title):
 
     # Read test statistics JSON and convert to HTML table
-    try:
-        test_statistics_path = hyperopt_stats_path
-        with open(test_statistics_path, "r") as f:
-            test_statistics = json.load(f)
-        test_statistics_html = "<h2>Hyperopt Statistics</h2>"
-        test_statistics_html = json_to_html_table(test_statistics)
-    except Exception as e:
-        LOG.info(f"Error reading hyperopt statistics: {e}")
+    # try:
+    #     test_statistics_path = hyperopt_stats_path
+    #     with open(test_statistics_path, "r") as f:
+    #         test_statistics = json.load(f)
+    #     test_statistics_html = "<h2>Hyperopt Statistics</h2>"
+    #     test_statistics_html += json_to_html_table(test_statistics)
+    # except Exception as e:
+    #     LOG.info(f"Error reading hyperopt statistics: {e}")
 
-    # Convert visualizations to HTML
     plots_html = ""
+    # Convert visualizations to HTML
+    hyperopt_hiplot_path = os.path.join(
+        viz_output_directory, "hyperopt_hiplot.html")
+    if os.path.isfile(hyperopt_hiplot_path):
+        with open(hyperopt_hiplot_path, "r", encoding="utf-8") as file:
+            hyperopt_hiplot_html = file.read()
+            plots_html += f'<div class="hiplot">{hyperopt_hiplot_html}</div>'
+
+    # Iterate through other files in viz_output_directory
     for plot_file in sorted(os.listdir(viz_output_directory)):
         plot_path = os.path.join(viz_output_directory, plot_file)
         if os.path.isfile(plot_path) and plot_file.endswith((".png", ".jpg")):
@@ -60,12 +66,10 @@ def generate_html_report(title):
         <h1>{title}</h1>
         <h2>Visualizations</h2>
         {plots_html}
-        {test_statistics_html}
     {get_html_closing()}
     """
 
     # Save the HTML report
-    title: str
     report_name = title.lower().replace(" ", "_")
     report_path = os.path.join(output_directory, f"{report_name}_report.html")
     with open(report_path, "w") as report_file:
