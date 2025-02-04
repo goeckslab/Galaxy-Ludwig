dd619f6c9e90abfce3572cfd57b23dc7fbdf7c69
paulocilasjr <pj_ibm@hotmail.com>
2025-02-04 18:18:01 -0500

    bag_processing

--- tools/ludwig_mil.py
+++ tools/ludwig_mil.py
@@ -4,6 +4,16 @@ import numpy as np
 import random
 import os
 
+def parse_bag_size(value):
+    """Parses bag_size argument to handle both single integers and ranges."""
+    if "-" in value:
+        min_val, max_val = map(int, value.split("-"))
+        if min_val > max_val:
+            raise argparse.ArgumentTypeError("Invalid range: min value cannot be greater than max value.")
+        return [min_val, max_val]
+    else:
+        return [int(value), int(value)]
+
 def load_csv(file_path):
     return pd.read_csv(file_path)
 
@@ -50,14 +60,36 @@ def split_data(metadata, split_proportions, dataleak=False):
 
     return metadata
 
-def create_bags_from_split(metadata, seed=42):
+def bag_turns(df_embeddings, bag_sizes):
+    embeddings_0 = df_embeddings.loc[df_embeddings["label"] == 0].values.tolist()
+    embeddings_1 = df_embeddings.loc[df_embeddings["label"] == 1].values.tolist()
+
+    np.random.shuffle(embeddings_0)
+    np.random.shuffle(embeddings_1)
+    bag_size = parse_bag_size(bag_sizes)
+
+    make_bag_1 = True
+
+    while len(embeddings_0) + len(embeddings_1) > 0:
+        print(bag_sizes)
+        bag = np.random.randint(bag_size[0], bag_size[1])
+
+
+def bag_random():
+    pass
+
+def bag_processing(embeddings, metadata, balance_enforced=False, bag_sizes=[3,5], seed=42):
     bags = []
     bag_set = set()
- 
-    for split in shuffled_metadata['split'].unique():
-        split_metada = metadata[metadata['split'] == split]
+    bags_sizes = parse_bag_size(bags_sizes)
 
-
+    for split in metadata['split'].unique():
+        split_metadata = metadata[metadata['split'] == split]
+        split_embeddings = pd.merge(split_metadata, embeddings, on='sample_name')
+        if balance_enforced:
+            bag_turns(split_embeddings, bag_sizes)
+        #else:
+            #bag_ramdom()
 
 def aggregate_max_pooling(embeddings):
     return np.max(embeddings, axis=0)
@@ -71,7 +103,6 @@ def aggregate_sum_pooling(embeddings):
 def convert_embedding_to_string(embedding):
     return ",".join(map(str, embedding))
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process CSV files to create bags of embeddings")
 
@@ -79,6 +110,9 @@ if __name__ == "__main__":
     parser.add_argument("--metadata_csv", type=str, required=True, help="The metadata in CSV file (Must contain 'sample_name' column and 'label' column.")
     parser.add_argument("--split_proportions", type=str, default='0.7,0.1,0.2', help="Proportions for train, validation, and test splits.")
     parser.add_argument("--dataleak", action="store_true", help="Prevents dataleak when splitting the data.")
+    parser.add_argument("--balance_enforced", action="store_true", help="Create bags in turns, reducing probability of large imbalacend bags")
+    parser.add_argument("--bag_size", type=str, required=True, help="Bag size as a single number (e.g., 4) or a range (e.g., 3-5).")    
+    parser.add_argument("--seed", type=int, help="seed number")
     #parser.add_argument("--output_csv", required=True, help="Path to the output CSV file")
 
     args = parser.parse_args()
@@ -92,8 +126,5 @@ if __name__ == "__main__":
             split_proportions = args.split_proportions, 
             dataleak = args.dataleak
         )
+    bag_processing(embeddings_data, metadata_csv, args.balance_enforced, args.bag_size, args.seed)
 
-
-
-
-    print(metadata_csv)
